898c07d6277ac6fd320b8f19afdcc65eec360c23
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-05-17 06:17:29 +0900

    feat: myMovie UI 구현
    코드 정리 및 컴포넌트 분리 필요

--- src/components/MyMovie.tsx
+++ src/components/MyMovie.tsx
@@ -3,43 +3,71 @@ import IcAllDropdown from '../asset/icon/ic_AllDropdown.svg';
 import IcGoButton from '../asset/icon/ic_GoButton.svg';
 import ICAddAudience from '../asset/icon/ic_AddAudience.svg';
 
+import { dummyMovieData } from '../constants/dummyUser';
+
 function MyMovie() {
   return (
     <MyMovieSection>
       <MyMovieHeader>
-        <MyMovieTitle>내가 본 영화</MyMovieTitle>
-        <MyMovieNum>8건</MyMovieNum>
+        <HeaderTextWrapper>
+          <MyMovieTitle>내가 본 영화</MyMovieTitle>
+          <MyMovieNum>{dummyMovieData.length}건</MyMovieNum>
+        </HeaderTextWrapper>
+
         <HeaderBtnWrapper>
           <DropDownBtn src={IcAllDropdown} />
           <GoBtn src={IcGoButton} />
           <AddAudienceBtn src={ICAddAudience} />
         </HeaderBtnWrapper>
       </MyMovieHeader>
+
+      <MovieCardWrapper>
+        {dummyMovieData.map((data) => {
+          return (
+            <MovieCard>
+              <MoviePoster src={data.imgSrc} />
+
+              <WatchedMovieInfo>
+                <WatchedMovieName>{data.name}</WatchedMovieName>
+                <WatchedMovieSubName>{data.subName}</WatchedMovieSubName>
+                <WatchedInfo>{data.date}</WatchedInfo>
+                <WatchedInfo>{data.cinemaInfo}</WatchedInfo>
+                <ReviewBtn src={data.reviewBtn ? data.reviewBtn : data.editBtn} id={data.editBtn ? 'edit' : ''} />
+                {data.deleteBtn ? <ReviewDelBtn src={data.deleteBtn} /> : <></>}
+              </WatchedMovieInfo>
+            </MovieCard>
+          );
+        })}
+      </MovieCardWrapper>
     </MyMovieSection>
   );
 }
 
-const MyMovieSection = styled.section``;
+const MyMovieSection = styled.section`
+  display: grid;
+
+  width: 94.5rem;
+
+  margin: -64rem 41.4rem 0rem 60rem;
+`;
 const MyMovieHeader = styled.header`
   display: flex;
   justify-content: space-between;
+
+  margin-left: 1.25rem;
+`;
+
+const HeaderTextWrapper = styled.div`
+  display: flex;
 `;
 const MyMovieTitle = styled.p`
   display: flex;
-  position: absolute;
-  top: 23.4rem;
-  left: 61.3rem;
-
-  font-weight: 500;
 
   font-size: ${({ theme }) => theme.fonts.Body2};
 `;
 
 const MyMovieNum = styled.p`
-  position: absolute;
-  top: 24.6rem;
-  left: 72.9rem;
-
+  margin-top: 2.45rem;
   margin-left: 0.9rem;
 
   color: ${({ theme }) => theme.colors.gray70};
@@ -48,11 +76,11 @@ const MyMovieNum = styled.p`
 
 const HeaderBtnWrapper = styled.div`
   display: flex;
-  position: absolute;
-  top: 25.4rem;
-  right: 41.4rem;
+  align-items: center;
 
   gap: 1rem;
+
+  margin-right: 4rem;
 `;
 
 const DropDownBtn = styled.img`
@@ -68,6 +96,71 @@ const GoBtn = styled.img`
 const AddAudienceBtn = styled.img`
   width: 11.7rem;
   height: 3.7rem;
+`;
+
+const MovieCardWrapper = styled.article`
+  display: flex;
+  flex-wrap: wrap;
+
+  margin-top: 3.1rem;
+`;
+
+const MovieCard = styled.div`
+  display: flex;
+  align-items: center;
+
+  width: 43.4rem;
+  height: 24.8rem;
+
+  border-radius: 2rem;
+
+  background-color: ${({ theme }) => theme.colors.white};
+  box-shadow: 0.375552rem 0.375552rem 1.87776rem rgba(102, 128, 153, 0.2);
+
+  margin: 1.25rem;
+`;
+
+const MoviePoster = styled.img`
+  width: 13.7rem;
+  height: 19.3rem;
+
+  margin-left: 2.7rem;
+`;
+
+const WatchedMovieInfo = styled.div`
+  height: 19.3rem;
+  margin: 0rem 2.7rem;
+`;
+
+const WatchedMovieName = styled.p`
+  margin: 0;
+
+  color: ${({ theme }) => theme.colors.black};
+  font-size: ${({ theme }) => theme.fonts.Body1};
+`;
+
+const WatchedMovieSubName = styled.p`
+  color: ${({ theme }) => theme.colors.gray70};
+  font-size: ${({ theme }) => theme.fonts.Body5};
+`;
+
+const WatchedInfo = styled.p`
+  color: ${({ theme }) => theme.colors.black};
+  font-size: ${({ theme }) => theme.fonts.Body5};
+`;
+
+const ReviewBtn = styled.img`
+  width: 9.9rem;
+  height: 3.7rem;
+
+  &.edit {
+    width: 9.1rem;
+  }
+`;
+const ReviewDelBtn = styled.img`
+  width: 9.1rem;
+  height: 3.7rem;
+  margin-left: 1rem;
 `;
 
 export default MyMovie;
