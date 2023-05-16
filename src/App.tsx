6f416aec500eae6775fcaec3cc3e3297f781a89c
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-05-17 03:19:42 +0900

    init: 필요 없는 코드 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,12 @@
-import { styled } from 'styled-components';
 import UserInfo from './components/UserInfo';
 
 function App() {
   return (
     <>
-      {/*<StHeading>Vite + React</StHeading> */}
       <UserInfo />
     </>
   );
 }
 
-const StHeading = styled.h1`
-  ${({ theme }) => theme.fonts.Heading}
-  color: ${({ theme }) => theme.colors.red2}
-`;
 
 export default App;
