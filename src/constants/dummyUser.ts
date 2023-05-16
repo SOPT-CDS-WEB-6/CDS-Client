89cba75f86b30e9e785bd7f7a977d9cef6180e8c
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-05-17 03:10:36 +0900

    chore: nav 바에 들어갈 상수 데이터 추가

--- src/constants/dummyUser.ts
+++ src/constants/dummyUser.ts
@@ -3,3 +3,19 @@ export const dummyUser = [
     name: '김나현',
   },
 ] as const;
+
+export const dummyData = [
+  {
+    num: 0,
+    text: '기대되는 영화',
+  },
+  {
+    num: 8,
+    text: '내가 본 영화',
+    class: 'selected',
+  },
+  {
+    num: 0,
+    text: '내가 쓴 평점',
+  },
+];
