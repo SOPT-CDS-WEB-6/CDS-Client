a864b947187e50f16801f466431cfccffbf0b4f2
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-05-17 19:22:51 +0900

    chore: footer 상수 데이터 파일

---
+++ src/constants/footerData.ts
@@ -0,0 +1,33 @@
+export const FOOTER_TOP_DATA = [
+  { content: '회사소개' },
+  { content: '지속가능경영' },
+  { content: 'IR' },
+  { content: '채용정보' },
+  { content: '광고/제휴/출점문의' },
+  { content: '이용약관' },
+  { content: '편성기준' },
+  {
+    content: '개인정보처리방침',
+    attr: 'underline',
+  },
+  { content: '법적고지' },
+  { content: '이메일주소무단수집거부' },
+  { content: '윤리경영' },
+  { content: '사이버감사실' },
+];
+
+export const FOOTER_BOTTOM_DATA = [
+  {
+    content: '(04377)서울특별시 용산구 한강대로 23길 55, 아이파크몰 6층(한강로동)',
+  },
+  {
+    content:
+      '대표이사 : 허민회 • 사업자등록번호: 104-81-45690 • 통신판매업신고번호 : 2017-서울용산-0662 사업자정보확인',
+  },
+  {
+    content: '호스팅사업자 : CJ올리브네트웍스 • 개인정보보호 책임자: 심준범 • 대표이메일 : cjcgvmaster@cj.net',
+  },
+  {
+    content: 'ⓒCJ CGV. All Rights Reserved',
+  },
+];
