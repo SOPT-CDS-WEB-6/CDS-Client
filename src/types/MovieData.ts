b5eb412613dc68d4ec5deacbc12a4cb267dbcb6a
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-05-17 16:23:10 +0900

    chore: 데이터 타입 정의

---
+++ src/types/MovieData.ts
@@ -0,0 +1,18 @@
+export type NavDataInfo = {
+  num: number;
+  text: string;
+  content?: string;
+  class?: string;
+  idx?: number;
+};
+
+export type MovieDataInfo = {
+  imgSrc: string;
+  name: string;
+  subName: string;
+  date: string;
+  cinemaInfo: string;
+  reviewBtn?: string;
+  editBtn?: string;
+  deleteBtn?: string;
+};
