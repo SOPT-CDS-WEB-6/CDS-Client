e3afc7353a1f79b3e95b02ab90a292788ae90bf0
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-05-17 16:25:16 +0900

    chore: UserPreference 내부 컴포넌트 분리

---
+++ src/components/PreferenceNav.tsx
@@ -0,0 +1,65 @@
+import { styled } from 'styled-components';
+import { NavDataInfo } from '../types/MovieData';
+
+export interface PreferenceNavProps {
+  data: NavDataInfo;
+}
+
+function PreferenceNav(props: PreferenceNavProps) {
+  const { data } = props;
+
+  return (
+    <StInfoWrapper className={data.class}>
+      <StNumInfo>{data.num}</StNumInfo>
+      {data.content ? <StPointerIcon>{data.content}</StPointerIcon> : <></>}
+      <StTextInfo>{data.text}</StTextInfo>
+    </StInfoWrapper>
+  );
+}
+
+const StInfoWrapper = styled.div`
+  display: grid;
+
+  width: 19.6rem;
+  height: 10.4rem;
+
+  margin-top: 1rem;
+
+  border: 0.1rem solid ${({ theme }) => theme.colors.gray40};
+  border-radius: 0.5rem;
+
+  &.selected {
+    background-color: ${({ theme }) => theme.colors.red2};
+    color: ${({ theme }) => theme.colors.white};
+  }
+`;
+
+const StNumInfo = styled.p`
+  display: flex;
+  justify-content: center;
+
+  margin-top: 1.5rem;
+
+  font-size: ${({ theme }) => theme.fonts.Title};
+`;
+
+const StPointerIcon = styled.p`
+  position: absolute;
+
+  margin-left: 2.4rem;
+
+  color: ${({ theme }) => theme.colors.white};
+
+  font-size: ${({ theme }) => theme.fonts.Title};
+`;
+
+const StTextInfo = styled.p`
+  display: flex;
+  justify-content: center;
+
+  margin-top: -3.3rem;
+
+  font-size: ${({ theme }) => theme.fonts.Body5};
+`;
+
+export default PreferenceNav;
