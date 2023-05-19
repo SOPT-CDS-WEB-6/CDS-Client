85bedfacd943281b40f4d24dee008293c7abf0a9
Areum-lab <80264647+Areum-lab@users.noreply.github.com>
2023-05-20 00:23:13 +0900

    refactor: 객체 배열 -> 문자열 배열로 수정

--- src/constants/footerData.ts
+++ src/constants/footerData.ts
@@ -14,17 +14,8 @@ export const FOOTER_TOP_DATA = [
 ];
 
 export const FOOTER_BOTTOM_DATA = [
-  {
-    content: '(04377)서울특별시 용산구 한강대로 23길 55, 아이파크몰 6층(한강로동)',
-  },
-  {
-    content:
-      '대표이사 : 허민회 • 사업자등록번호: 104-81-45690 • 통신판매업신고번호 : 2017-서울용산-0662 사업자정보확인',
-  },
-  {
-    content: '호스팅사업자 : CJ올리브네트웍스 • 개인정보보호 책임자: 심준범 • 대표이메일 : cjcgvmaster@cj.net',
-  },
-  {
-    content: 'ⓒCJ CGV. All Rights Reserved',
-  },
+  '(04377)서울특별시 용산구 한강대로 23길 55, 아이파크몰 6층(한강로동)',
+  '대표이사 : 허민회 • 사업자등록번호: 104-81-45690 • 통신판매업신고번호 : 2017-서울용산-0662 사업자정보확인',
+  '호스팅사업자 : CJ올리브네트웍스 • 개인정보보호 책임자: 심준범 • 대표이메일 : cjcgvmaster@cj.net',
+  'ⓒCJ CGV. All Rights Reserved',
 ];
