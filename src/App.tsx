baf77260a84a43fd350d1f9c340f3aefaddb2c08
SynthiaLee <seanne0801@gmail.com>
2023-05-23 14:34:40 +0900

    chore: MovieInfoPage 생성 및 main 에서 global style 적용

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,9 @@
-import MovieInfo from './components/MovieInfo';
-import { GlobalStyle } from './styles/globalStyles';
-import MovieDetailBox from './components/MovieDetailBox';
+import MovieInfoPage from './pages/MovieInfoPage';
 
 function App() {
   return (
     <>
-      <GlobalStyle />
-      <MovieInfo />
-      <MovieDetailBox />
+      <MovieInfoPage />
     </>
   );
 }
---
+++ src/pages/MovieInfoPage.tsx
@@ -0,0 +1,69 @@
+import styled from 'styled-components';
+import MovieDetail from '../components/MovieDetail';
+import SideAdBanner from '../components/SideAdBanner';
+import TrailerBox from '../components/TrailerBox';
+import StillCutBox from '../components/StillCutBox';
+import ScoreBox from '../components/ScoreBox';
+import MovieInfo from '../components/MovieInfo';
+
+function MovieInfoPage() {
+  return (
+    <StMovieInfoBox>
+      <StFirstBox>
+        <MovieInfo />
+        <StSecondBox>
+          <MovieDetail />
+          <SideAdBanner />
+        </StSecondBox>
+      </StFirstBox>
+      <StFourthBox>
+        <StThirdBox>
+          <TrailerBox />
+          <StillCutBox />
+          <ScoreBox />
+        </StThirdBox>
+        <StBlankBox />
+      </StFourthBox>
+    </StMovieInfoBox>
+  );
+}
+
+export default MovieInfoPage;
+
+const StMovieInfoBox = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  margin-top: 4.9rem;
+`;
+
+const StFirstBox = styled.section`
+  display: flex;
+  flex-direction: column;
+
+  width: 119.6em;
+`;
+
+const StSecondBox = styled.section`
+  display: flex;
+  justify-content: center;
+
+  margin-top: 14.2rem;
+`;
+
+const StThirdBox = styled.section`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  width: 99.4rem;
+`;
+
+const StFourthBox = styled.section`
+  display: flex;
+`;
+
+const StBlankBox = styled.section`
+  width: 22.7rem;
+`;
