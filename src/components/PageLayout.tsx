18223cc7bbaa291b503e402b0d91a20a0dd92b4a
gunom <gu3062@naver.com>
2023-05-23 22:37:06 +0900

    feat: pageLayout commonent 및 Router 추가

---
+++ src/components/PageLayout.tsx
@@ -0,0 +1,13 @@
+const PageLayout = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <div className="page-layout">
+      {/* header */}
+      {/* scroll_header */}
+      {/* stickyBtn */}
+      {/* footer */}
+      {children}
+    </div>
+  );
+};
+
+export default PageLayout;
